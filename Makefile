73b3bf8a1b3f78244f67a2724a01c488b6c99a6c
catterer <catterer@yandex.ru>
2017-03-23 13:40:12 +0300

    reverse

--- Makefile
+++ Makefile
@@ -1,9 +1,11 @@
-all: braces gen_braces
+all: braces gen_braces reverse
 
 braces: braces.cc
 	g++ braces.cc -std=c++1y -Wall -o braces
 gen_braces: gen_braces.cc
 	g++ gen_braces.cc -std=c++1y -Wall -o gen_braces
+reverse: reverse.c
+	gcc reverse.c -Wall -o reverse
 
 clean:
 	rm -rf braces gen_braces
---
+++ reverse.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv) {
+    if (argc < 2)
+        exit(EXIT_FAILURE);
+    char* s = strdup(argv[1]);
+    size_t l = strlen(s);
+    size_t i;
+    for (i = 0; i < l/2; i++) {
+        if (i == l-i-1)
+            break;
+        s[i] ^= s[l-i-1];
+        s[l-i-1] ^= s[i];
+        s[i] ^= s[l-i-1];
+    }
+    printf("%s\n", s);
+    free(s);
+    return 0;
+}
